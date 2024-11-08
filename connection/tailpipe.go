99eb7c111a19a9df0b3913c11b45bb54614df8b2
kai <kai@turbot.com>
2024-11-08 17:26:29 +0700

    tailpipe connection invokes tailpipe connect, passing time range

--- connection/tailpipe.go
+++ connection/tailpipe.go
@@ -14,38 +14,13 @@ import (
 
 const TailpipeConnectionType = "tailpipe"
 
-type TailpipeDatabaseFilters struct {
-	// partition wildcards
-	Partitions []string
-	// the indexes to include
-	Indexes []string
-	// the data range
-	From *time.Time
-	To   *time.Time
-}
-
-func (o *TailpipeDatabaseFilters) Equals(other *TailpipeDatabaseFilters) bool {
-	if (o == nil) != (other == nil) ||
-		!slices.Equal(o.Partitions, other.Partitions) ||
-		!slices.Equal(o.Indexes, other.Indexes) ||
-		(o.From == nil) != (other.From == nil) ||
-		o.From != nil && !o.From.Equal(*other.From) ||
-		(o.To == nil) != (other.To == nil) ||
-		o.To != nil && !o.To.Equal(*other.To) {
-		return false
-	}
-
-	return true
-}
-
 type TailpipeConnection struct {
 	ConnectionImpl
 
 	From *string `cty:"from" hcl:"from"`
 	To   *string `cty:"to" hcl:"to"`
 	// if an option is passed to GetConnectionString, it may override the From and To values
-	OverrideFrom *string
-	OverrideTo   *string
+	OverrideFilters *TailpipeDatabaseFilters
 
 	// store the current db filename, along with the db options used to create it
 	// when GetConnecitonString is called, if we have a db filename already _and the options are the same_
@@ -59,6 +34,7 @@ func NewTailpipeConnection(shortName string, declRange hcl.Range) PipelingConnec
 		ConnectionImpl: NewConnectionImpl(TailpipeConnectionType, shortName, declRange),
 	}
 }
+
 func (c *TailpipeConnection) GetConnectionType() string {
 	return TailpipeConnectionType
 }
@@ -74,7 +50,7 @@ func (c *TailpipeConnection) Resolve(ctx context.Context) (PipelingConnection, e
 }
 
 func (c *TailpipeConnection) Validate() hcl.Diagnostics {
-
+	// todo validate From and To
 	return nil
 }
 
@@ -82,8 +58,19 @@ func (c *TailpipeConnection) GetConnectionString(opts ...ConnectionStringOpt) st
 	for _, opt := range opts {
 		opt(c)
 	}
+	args := []string{"connect"}
+
+	// resolve the filters
+	filters := c.getFilters()
+	if from := filters.From; from != nil {
+		args = append(args, "--from", from.Format(time.RFC3339))
+	}
+	if to := filters.To; to != nil {
+		args = append(args, "--to", to.Format(time.RFC3339))
+	}
+
 	// Invoke the "tailpipe connect" shell command and capture output
-	cmd := exec.Command("tailpipe", "connect")
+	cmd := exec.Command("tailpipe", args...)
 
 	// Run the command and get the output
 	output, err := cmd.Output()
@@ -96,10 +83,10 @@ func (c *TailpipeConnection) GetConnectionString(opts ...ConnectionStringOpt) st
 	connectionString := strings.TrimSpace(string(output))
 	return connectionString
 }
+
 func (c *TailpipeConnection) GetEnv() map[string]cty.Value {
 	return map[string]cty.Value{}
 }
-
 func (c *TailpipeConnection) Equals(otherConnection PipelingConnection) bool {
 	// If both pointers are nil, they are considered equal
 	if c == nil && helpers.IsNil(otherConnection) {
@@ -135,14 +122,72 @@ func (c *TailpipeConnection) CtyValue() (cty.Value, error) {
 	return ctyValueForConnection(c)
 }
 
-// SetTimeRange sets the time range for the connection
-func (c *TailpipeConnection) SetTimeRange(from, to *time.Time) {
-	if from != nil {
-		fromStr := from.Format(time.RFC3339)
-		c.OverrideFrom = &fromStr
+func (c *TailpipeConnection) setFilters(f TailpipeDatabaseFilters) {
+	c.OverrideFilters = &f
+}
+
+// resolve the active filters, either from the connection or the override
+func (c *TailpipeConnection) getFilters() *TailpipeDatabaseFilters {
+	var res = &TailpipeDatabaseFilters{}
+	if c.From != nil {
+		// we have already validated the time format
+		from, _ := time.Parse(time.RFC3339, *c.From)
+		res.From = &from
 	}
-	if to != nil {
-		toStr := to.Format(time.RFC3339)
-		c.OverrideTo = &toStr
+	if c.To != nil {
+		// we have already validated the time format
+		to, _ := time.Parse(time.RFC3339, *c.To)
+		res.To = &to
 	}
+
+	// if we have overrides, use them
+	if c.OverrideFilters != nil {
+		if c.OverrideFilters.From != nil {
+			if from := res.From; from == nil || from.Before(*c.OverrideFilters.From) {
+				res.From = c.OverrideFilters.From
+			}
+		}
+		if overrideTo := c.OverrideFilters.To; overrideTo != nil {
+			if to := res.To; to == nil || to.Before(*overrideTo) {
+				res.To = overrideTo
+			}
+		}
+	}
+	// TODO partitions and indexes
+
+	return res
+}
+
+// option to set the filter for the connection
+func WithFilter(f TailpipeDatabaseFilters) ConnectionStringOpt {
+	return func(c ConnectionStringProvider) {
+		// if this connection supports time range, set it
+		if c, ok := c.(*TailpipeConnection); ok {
+			c.setFilters(f)
+		}
+	}
+}
+
+type TailpipeDatabaseFilters struct {
+	// partition wildcards
+	Partitions []string
+	// the indexes to include
+	Indexes []string
+	// the data range
+	From *time.Time
+	To   *time.Time
+}
+
+func (o *TailpipeDatabaseFilters) Equals(other *TailpipeDatabaseFilters) bool {
+	if (o == nil) != (other == nil) ||
+		!slices.Equal(o.Partitions, other.Partitions) ||
+		!slices.Equal(o.Indexes, other.Indexes) ||
+		(o.From == nil) != (other.From == nil) ||
+		o.From != nil && !o.From.Equal(*other.From) ||
+		(o.To == nil) != (other.To == nil) ||
+		o.To != nil && !o.To.Equal(*other.To) {
+		return false
+	}
+
+	return true
 }
